5385aff48e5f2c4adc42a024b87398edfb51e5a9
Paton Vinal <paton.vinal@gmail.com>
2017-06-14 22:52:06 -0400

    eslinted and added no results feature

--- app.js
+++ app.js
@@ -34,97 +34,97 @@ function addSearch(state, response) {
 function emptyState(state = appState){
   state.search = '',
     state.availableDrivers = [],
-    state.availableLoads = []
+    state.availableLoads = [];
 }
 
-function queryDataBase(search, pageURL = "http://localhost:8080/"){
+function queryDataBase(search, pageURL = 'http://localhost:8080/'){
 
   if($('#selectorId').val() === 'driver') {
 
-    fetch(`http://localhost:8080/drivers`).then(response => {
-       return response.json()
-     })
+    fetch('http://localhost:8080/drivers').then(response => {
+      return response.json();
+    })
         .then(data =>{
-          return addDrivers(appState, data)
+          return addDrivers(appState, data);
         })
         .then(function(){
-          render(($("div.real-data")));
+          render(($('div.real-data')));
         });
   }else {
-      fetch(`http://localhost:8080/brokershippers`).then(response => {
-       return response.json()
-     })
+    fetch('http://localhost:8080/brokershippers').then(response => {
+      return response.json();
+    })
         .then(data =>{
-          return addLoad(appState, data)
+          return addLoad(appState, data);
         })
         .then(function(){
-          render(($("div.real-data")));
+          render(($('div.real-data')));
         });
   }
 }
 
-function postDriver(search, pageURL = "http://localhost:8080/"){
+function postDriver(search, pageURL = 'http://localhost:8080/'){
     
-    let data = {
-        companyName: $('#companyName').val(),
-        fleetManager: {
-            firstName: $('#firstName').val(),
-            lastName: $('#lastName').val()
-        },
-        phoneNum: $('#phoneNum').val(),
-        truck: {
-            truckNum: $('#truckNum').val(),
-            trailerNum: $('#trailerNum').val(),
-            trailerType: $('#trailerType').val(),
-            location: $('#location').val()
-        }
-    };
+  let data = {
+    companyName: $('#companyName').val(),
+    fleetManager: {
+      firstName: $('#firstName').val(),
+      lastName: $('#lastName').val()
+    },
+    phoneNum: $('#phoneNum').val(),
+    truck: {
+      truckNum: $('#truckNum').val(),
+      trailerNum: $('#trailerNum').val(),
+      trailerType: $('#trailerType').val(),
+      location: $('#location').val()
+    }
+  };
 
-    fetch(`http://localhost:8080/drivers`, {
-        method: 'post', 
-        headers: { 
-            'Accept': 'application/json, text/plain, /', 
-            'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    }).then(response => {
-       return response.json()
-     })
+  fetch('http://localhost:8080/drivers', {
+    method: 'post', 
+    headers: { 
+      'Accept': 'application/json, text/plain, /', 
+      'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }).then(response => {
+    return response.json();
+  })
         .then(data =>{
-            console.log(data);
-            queryDataBase();
+          console.log(data);
+          queryDataBase();
         })
         .then(function(){
-          render(($("div.real-data")));
+          render(($('div.real-data')));
         });
 }
 
-function postBroker(search, pageURL = "http://localhost:8080/"){
+function postBroker(search, pageURL = 'http://localhost:8080/'){
     
-    let data = {
-        companyName: $('#brokerName').val(),
-        phone: $('#phone').val(),
-        load: {
-            puLocation: $('#puLocation').val(),
-            delLocation: $('#delLocation').val(),
-            pudate: $('#pudate').val(),
-            freight: $('#freight').val()
-        }
-    };
+  let data = {
+    companyName: $('#brokerName').val(),
+    phone: $('#phone').val(),
+    load: {
+      puLocation: $('#puLocation').val(),
+      delLocation: $('#delLocation').val(),
+      pudate: $('#pudate').val(),
+      freight: $('#freight').val()
+    }
+  };
 
-    fetch(`http://localhost:8080/brokershippers`, {
-        method: 'post', 
-        headers: { 
-            'Accept': 'application/json, text/plain, /', 
-            'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    }).then(response => {
-       return response.json()
-     })
+  fetch('http://localhost:8080/brokershippers', {
+    method: 'post', 
+    headers: { 
+      'Accept': 'application/json, text/plain, /', 
+      'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }).then(response => {
+    return response.json();
+  })
         .then(data =>{
-            queryDataBase();
+          queryDataBase();
         })
         .then(function(){
-          render(($("div.real-data")));
+          render(($('div.real-data')));
         });
 }
 
@@ -141,7 +141,7 @@ function render(element){
   let html = '';
   if(appState.availableDrivers.length > 0) {
     appState.availableDrivers.forEach(driver => {
-        html += `
+      html += `
                     <div class='entry'>
                         <h3><u>${driver.companyName}</u></h3>
                         <p>Contact: ${driver.name}</p>
@@ -151,12 +151,12 @@ function render(element){
                         <p>Trailer Type: ${driver.truckInfo[0].trailerType}, Number: ${driver.truckInfo[0].trailerNum}</p>
                         <p>Truck Number: ${driver.truckInfo[0].truckNum}</p>
                     </div>`;
-      });
+    });
         
   } else if(appState.availableLoads.length >0) {
-      appState.availableLoads.forEach(job => {
-        let date = new Date(job.load.pudate);
-        html +=`
+    appState.availableLoads.forEach(job => {
+      let date = new Date(job.load.pudate);
+      html +=`
             <div class='entry'>
                 <h3><u>${job.companyName}</u></h3>
                 <p>Phone: ${job.phone}</p>
@@ -167,10 +167,12 @@ function render(element){
                 <p>Delivery Location: ${job.load.delLocation}</p>
             </div>`;
         
-      }); 
-    } else {
-      console.log("whelp it's empty");
-    }
+    }); 
+  } else {
+    html +=`
+        <div>Sorry, no information found. Please try another search.</div>
+        `;
+  }
 
   element.html(html);
 	//element.removeClass("hidden");
@@ -195,18 +197,18 @@ $(function(){
 
   $('form.search').submit(event => {
     event.preventDefault();
-	queryDataBase();
-  })
+    queryDataBase();
+  });
 
-   $('#driver-post').submit(event => {
+  $('#driver-post').submit(event => {
     event.preventDefault();
-	postDriver();
-  })
+    postDriver();
+  });
 
   $('#broker-post').submit(event => {
     event.preventDefault();
-	postBroker();
-  })
+    postBroker();
+  });
 
 
-})
+});

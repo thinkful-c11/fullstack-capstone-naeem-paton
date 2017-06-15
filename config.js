e91c2600fe7463f6f99baa11ca7e3291c60e53c5
PROB8 <blackb8r@gmail.com>
2017-06-15 19:12:05 -0400

    url updates

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
+                       global.DATABASE_URL 
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                        global.TEST_DATABASE_URL ||
